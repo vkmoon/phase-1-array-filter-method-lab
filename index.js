c838ffff41b38b60149f99585988ada8ea48dce8
Valeria <valeriakirkbride@gmail.com>
2023-09-04 18:12:14 -0600

    filter method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, match){
+    const names = drivers.filter(driver => driver.toUpperCase() === match.toUpperCase())
+    return names
+}
+
+function fuzzyMatch(drivers, match){
+    const names = drivers.filter(driver => driver.startsWith(match))
+    return names
+}
+
+function  matchName(drivers, string){
+    const result = drivers.filter(driver => driver.name === string)
+    return result
+
+}
+
+
+
